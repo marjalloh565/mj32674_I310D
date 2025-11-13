b559550e740515f78bfd4a4e0ef7af47e364800c
Mariam Jalloh <mariamjalloh@wireless-10-146-96-227.public.utexas.edu>
2025-11-12 20:34:43 -0600

    Updated volume_of_sphere.py variable name from area to volume

--- volume_of_sphere.py
+++ volume_of_sphere.py
@@ -6,9 +6,9 @@ def calculate_volume_of_sphere(radius):
 
 
 radius1 = 30
-area1 = calculate_volume_of_sphere(radius1)
+volume1 = calculate_volume_of_sphere(radius1)
 print(f"The volume of a sphere with radius {radius1} is: {volume1}")
 
 radius2 = 40
-area2 = calculate_volume_of_sphere(radius2)
+volume2 = calculate_volume_of_sphere(radius2)
 print(f"The volume of a sphere with radius {radius2} is: {volume2}")
